2f7d25d1c39d8ff55f3b303bd6ecec217a6a8975
Jahn Veach <j@hnvea.ch>
2014-11-16 16:25:55 -0600

    polymorphic rotateLeft and rotateRight

--- CubieCube.hs
+++ CubieCube.hs
@@ -53,3 +53,20 @@ getUr2Df cc = 1
 choose :: (RealFrac a, Enum a) => a -> a -> Int
 choose n k  = (fromIntegral $ truncate p) :: Int
     where p = product [(n + 1 - i) / i | i <- [1..k]]
+
+slice :: Int -> Int -> [a] -> [a]
+slice a b xs = take (b-a+1) . drop a $ xs
+
+takeR :: Int -> [a] -> [a]
+takeR n l = go (drop n l) l
+    where go []     r      = r
+          go (_:xs) (_:ys) = go xs ys
+
+rotate :: ([a] -> [a]) -> Int -> Int -> [a] -> [a]
+rotate f a b xs = (take a xs) ++ (f $ slice a b xs) ++ (takeR (length xs - b - 1) xs)
+
+rotateLeft :: Int -> Int -> [a] -> [a]
+rotateLeft = rotate (\xs -> tail xs ++ [head xs])
+
+rotateRight:: Int -> Int -> [a] -> [a]
+rotateRight = rotate (\xs -> last xs : init xs)
